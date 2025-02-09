d6adaf9b11fa5b96a602737d50da0e90c3f5e66c
Djihane Mahraz <60300310@udst.edu.qa>
2025-02-09 13:45:55 +0300

    started the new lab, added the functions with processing and threading, need to change the calculations for amdhal and gustaffson

--- src/.ipynb_checkpoints/processes-checkpoint.py
+++ src/.ipynb_checkpoints/processes-checkpoint.py
@@ -1,51 +1,22 @@
 import time
 import multiprocessing
-from src.utils import join_random_letters
-from src.utils import add_random_numbers
+from src.utils import add_n_numbers
 
-def run_process(num_letters = 10000, num_numbers = 10000):
+def run_process(num_processes = 4, num_numbers = 10000):
     """
-    Executes parallel processing for generating random letters and summing random numbers.
-
-    This function uses multiprocessing to run two separate processes for each task:
-    - `join_random_letters`: Generates and joins random letters in two separate processes.
-    - `add_random_numbers`: Generates and sums random numbers in two separate processes.
-
-    The function measures and returns the total execution time.
-
-    Args:
-        num_letters (int, optional): The total number of random letters to generate. Default is 10,000.
-        num_numbers (int, optional): The total number of random numbers to generate. Default is 10,000.
-
-    Returns:
-        float: The total execution time in seconds.
     """
-    
+    # start processes in a loop
+    processes = []
+    step = num_numbers // num_processes
     # calculating time for processes 
-    total_start_time = time.time()
-    # Create processes for both functions
-    process_letters1 = multiprocessing.Process(target=join_random_letters, args=(0, num_letters//2))
-    process_letters2 = multiprocessing.Process(target=join_random_letters, args=(num_letters//2, num_letters))
-    
-    process_numbers1 = multiprocessing.Process(target=add_random_numbers, args=(0, num_numbers//2))
-    process_numbers2 = multiprocessing.Process(target=add_random_numbers, args=(num_numbers//2, num_numbers))
-    
-    # Start the processes
-    process_letters1.start()
-    process_letters2.start()
-    # process_letters3.start()
-    
-    process_numbers1.start()
-    process_numbers2.start()
-    # process_numbers3.start()
-    # Wait for all processes to complete
-    process_letters1.join()
-    process_letters2.join()
-    # process_letters3.join()
-    
-    process_numbers1.join()
-    process_numbers2.join()
-    # process_numbers3.join()
-    
-    total_end_time = time.time()
-    return total_end_time - total_start_time
+    start_time = time.time()
+    for i in range(num_processes):
+        process = multiprocessing.Process(target=add_n_numbers, args=(i * step, ((i+1) * step) - 1))
+        processes.append(process)
+        process.start()
+    # wait for threads to finish
+    for process in processes:
+        process.join()
+    end_time = time.time()
+    return end_time - start_time
+
--- src/.ipynb_checkpoints/sequential-checkpoint.py
+++ src/.ipynb_checkpoints/sequential-checkpoint.py
@@ -1,26 +1,10 @@
 import time
-from src.utils import join_random_letters
-from src.utils import add_random_numbers
+from src.utils import add_n_numbers
 
-def run_sequential(num_letters = 10000, num_numbers = 10000):
+def run_sequential(num_numbers = 10000):
     """
-    Executes random letter generation and number summation sequentially.
-
-    This function runs the following tasks one after the other:
-    - `join_random_letters`: Generates and joins random letters.
-    - `add_random_numbers`: Generates and sums random numbers.
-
-    The function measures and returns the total execution time.
-
-    Args:
-        num_letters (int, optional): The total number of random letters to generate. Default is 10,000.
-        num_numbers (int, optional): The total number of random numbers to generate. Default is 10,000.
-
-    Returns:
-        float: The total execution time in seconds.
     """
     total_start_time = time.time()
-    join_random_letters(0, num_letters)
-    add_random_numbers(0, num_numbers)
+    add_n_numbers(0, num_numbers)
     total_end_time = time.time()
     return total_end_time - total_start_time
